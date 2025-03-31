c2dd39b2d3d7769f3b065dab5b136d9351997785
DaniAkiode <dannyakiode@hotmail.co.uk>
2025-03-31 16:17:15 +0100

    Random Rainforest fixed, now the user can choose between models

--- model.py
+++ model.py
@@ -1,20 +1,20 @@
 import joblib
 
 # Load vectorizer
-vectorizer = joblib.load("cv3.pkl")
-TFVectorizer = joblib.load("tfidf.pkl")
+cv = joblib.load("cv3.pkl")
+tfidf = joblib.load("tfidf.pkl")
 
 # Load multiple models
 models = {
-    "naive_bayes": joblib.load("NBSpamDetector3.pkl"),
-    "random_forest": joblib.load("rfBasedSpamDetector.pkl"),  
+    "naive_bayes": (cv, joblib.load("NBSpamDetector3.pkl")),
+    "random_forest": (tfidf, joblib.load("rfBasedSpamDetector.pkl")),  
 }
 
 def predict_spam(text, model_name="naive_bayes"):  # Default to Naive Bayes
     if model_name not in models:
         return "Invalid model selected!"
 
-    model = models[model_name]  # Get selected model
+    vectorizer, model = models[model_name]  # Get selected model
     text_vector = vectorizer.transform([text])  # Convert text to numerical format
     prediction = model.predict(text_vector)  # Make a prediction
     
